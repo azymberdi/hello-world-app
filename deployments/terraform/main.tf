369dd8858be5e92f140d2e02c05aa518e940b651
azymberdi <azymberdi@gmail.com>
2020-09-04 20:54:11 -0400

    main.tf added

---
+++ deployments/terraform/main.tf
@@ -0,0 +1,30 @@
+module "academy-deploy" {
+  source  = "fuchicorp/chart/helm"
+  deployment_name        = "hello-world"
+  deployment_environment = "${var.deployment_environment}"
+  deployment_endpoint    = "${lookup(var.deployment_endpoint, "${var.deployment_environment}")}.${var.google_domain_name}"
+  deployment_path        = "hello-world"
+  template_custom_vars  = {     
+    deployment_image     = "${var.deployment_image}"
+  }
+}
+
+variable "deployment_image" {
+    default = "docker.turkmendevops.com/hello-app-dev-feature:ad92483"
+
+variable "deployment_environment" {
+    default = "dev"
+}
+variable "deployment_endpoint" {
+    type = "map"
+     default = {
+        test  = "test.hello"
+        dev  = "dev.hello"
+        qa   = "qa.hello"
+        prod = "hello"
+        stage = "stage.hello"
+  }
+}
+variable "google_domain_name" {
+    default = "turkmendevops.com"
+}
